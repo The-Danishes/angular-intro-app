c9259b44a3ee6c11c45e499b4056783a2a9a389a
Danish Mughal <mughal.danish@gmail.com>
2016-12-22 18:24:12 -0800

    ng-repeat

--- app/assets/javascripts/pageCtrl.js
+++ app/assets/javascripts/pageCtrl.js
@@ -1,8 +1,12 @@
+/* global angular */
+
 (function() {
   "use strict";
 
   angular.module("app").controller("pageCtrl", function($scope) {
     $scope.message = "Hello!";
+
+    $scope.reviews = ["Angular is the best!", "Angular is the coolest!", "I love angular"];
 
     window.$scope = $scope;
   });
