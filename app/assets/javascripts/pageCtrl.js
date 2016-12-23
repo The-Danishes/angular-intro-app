0ec66dd98fa5613967616cfd71992f2a273ebf48
Danish Mughal <mughal.danish@gmail.com>
2016-12-22 18:44:17 -0800

    ng-click to add new reviews

--- app/assets/javascripts/pageCtrl.js
+++ app/assets/javascripts/pageCtrl.js
@@ -8,6 +8,10 @@
 
     $scope.reviews = ["Angular is the best!", "Angular is the coolest!", "I love angular"];
 
+    $scope.addReview = function(review) {
+      $scope.reviews.push(review);
+    };
+
     window.$scope = $scope;
   });
 
